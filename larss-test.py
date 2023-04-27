70b1e74ff4139783fb99eab83f9ba82ab2b4a6ac
LarssJ <larss.jakobsons@icloud.com>
2023-04-27 21:14:39 +0300

    Autowrap addition

--- larss-test.py
+++ larss-test.py
@@ -1,26 +1,41 @@
 import pygame
+from pygame.locals import *
+from pygame import *
 import sys
 from time import sleep
 
+from data.drawText import drawText
+
 pygame.init()
 screen = pygame.display.set_mode((600, 400))
-pygame.display.set_caption("Pygame window ig")
+pygame.display.set_caption("Dating sim")
+
 while True:
-    # make text that says "Hello world!"
     font = pygame.font.Font(None, 30)
+    # textpos = text.get_rect()
+    # textpos.centerx = screen.get_rect().centerx
+    # textpos.centery = screen.get_rect().centery - 145
+    # screen.blit(text, textpos)
+
+    # create a rectangle that is hollow and white
+    pygame.draw.rect(screen, (255, 255, 255), (50, 10, 500, 100), 3)
+
+    text = drawText(
+        screen,
+        "Omagawd it actually wraps! Could you even imagine that? I mean, I can't. I'm just crazy asf ik! What if it goes out of border? I dont think its possible but lets test :D. Not there yet! Still writing. I think this is enough.",
+        (255, 255, 255),
+        (60, 15, 450, 90),
+        pygame.font.Font(None, int(450 * 450 / 500)),
+    )
+
     text = font.render(
-        "Wow look at this very long dialouge i sure hope it doesnt go off screen",
+        text,
         1,
         (255, 255, 255),
     )
-    textpos = text.get_rect()
-    textpos.centerx = screen.get_rect().centerx
-    textpos.centery = screen.get_rect().centery - 145
-    screen.blit(text, textpos)
-    # create a rectangle that is hollow and white
-    pygame.draw.rect(screen, (255, 255, 255), (50, 10, 500, 100), 3)
-    # update the display
+
     pygame.display.update()
+
     # loop through the events
     for event in pygame.event.get():
         # quit if the quit button was pressed
